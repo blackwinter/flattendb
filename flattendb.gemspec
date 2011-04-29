3ac63f65dbf1bf5d13a66d3206fdfd76a1efec08
Jens Wille <jens.wille@uni-koeln.de>
2011-04-29 15:24:46 +0200

    v0.0.7

--- flattendb.gemspec
+++ flattendb.gemspec
@@ -2,28 +2,27 @@
 
 Gem::Specification.new do |s|
   s.name = %q{flattendb}
-  s.version = "0.0.6"
+  s.version = "0.0.7"
 
   s.required_rubygems_version = Gem::Requirement.new(">= 0") if s.respond_to? :required_rubygems_version=
   s.authors = ["Jens Wille"]
-  s.date = %q{2010-03-15}
+  s.date = %q{2011-04-29}
   s.description = %q{Flatten relational databases.}
   s.email = %q{jens.wille@uni-koeln.de}
   s.executables = ["flattendb", "flattendb.mdb", "flattendb.mysql"]
-  s.extra_rdoc_files = ["COPYING", "ChangeLog", "README"]
+  s.extra_rdoc_files = ["README", "COPYING", "ChangeLog"]
   s.files = ["lib/flattendb.rb", "lib/flattendb/cli.rb", "lib/flattendb/version.rb", "lib/flattendb/base.rb", "lib/flattendb/types/mysql.rb", "lib/flattendb/types/mdb.rb", "bin/flattendb", "bin/flattendb.mdb", "bin/flattendb.mysql", "README", "ChangeLog", "Rakefile", "COPYING", "example/mysql-sample.flat.xml", "example/mysql-sample2flat.yaml", "example/mysql-sample.xml", "example/mysql-sample.sql"]
   s.homepage = %q{http://prometheus.rubyforge.org/flattendb}
-  s.rdoc_options = ["--charset", "UTF-8", "--title", "flattendb Application documentation", "--main", "README", "--line-numbers", "--inline-source", "--all"]
+  s.rdoc_options = ["--line-numbers", "--main", "README", "--charset", "UTF-8", "--all", "--title", "flattendb Application documentation (v0.0.7)"]
   s.require_paths = ["lib"]
   s.rubyforge_project = %q{prometheus}
-  s.rubygems_version = %q{1.3.6}
+  s.rubygems_version = %q{1.7.2}
   s.summary = %q{Flatten relational databases.}
 
   if s.respond_to? :specification_version then
-    current_version = Gem::Specification::CURRENT_SPECIFICATION_VERSION
     s.specification_version = 3
 
-    if Gem::Version.new(Gem::RubyGemsVersion) >= Gem::Version.new('1.2.0') then
+    if Gem::Version.new(Gem::VERSION) >= Gem::Version.new('1.2.0') then
       s.add_runtime_dependency(%q<highline>, [">= 0"])
       s.add_runtime_dependency(%q<libxml-ruby>, [">= 0"])
       s.add_runtime_dependency(%q<builder>, [">= 0"])
--- lib/flattendb/version.rb
+++ lib/flattendb/version.rb
@@ -1,38 +1,10 @@
-#--
-###############################################################################
-#                                                                             #
-# A component of flattendb, the relational database flattener.                #
-#                                                                             #
-# Copyright (C) 2007-2011 University of Cologne,                              #
-#                         Albertus-Magnus-Platz,                              #
-#                         50923 Cologne, Germany                              #
-#                                                                             #
-# Authors:                                                                    #
-#     Jens Wille <jens.wille@uni-koeln.de>                                    #
-#                                                                             #
-# flattendb is free software; you can redistribute it and/or modify it under  #
-# the terms of the GNU Affero General Public License as published by the Free #
-# Software Foundation; either version 3 of the License, or (at your option)   #
-# any later version.                                                          #
-#                                                                             #
-# flattendb is distributed in the hope that it will be useful, but WITHOUT    #
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or       #
-# FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License #
-# for more details.                                                           #
-#                                                                             #
-# You should have received a copy of the GNU Affero General Public License    #
-# along with flattendb. If not, see <http://www.gnu.org/licenses/>.           #
-#                                                                             #
-###############################################################################
-#++
-
 module FlattenDB
 
   module Version
 
     MAJOR = 0
     MINOR = 0
-    TINY  = 6
+    TINY  = 7
 
     class << self
 
