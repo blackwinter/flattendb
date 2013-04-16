3e1852c574a3bc4e8849f2755be503d01778ce68
Jörg Koch <joerg.koch@uni-koeln.de>
2013-04-16 14:45:06 +0200

    lib/flattendb/base.rb: [FIX] Select root and config from array's first key value pair.

--- lib/flattendb/base.rb
+++ lib/flattendb/base.rb
@@ -76,7 +76,7 @@ module FlattenDB
       config = options.select { |k, _| k.is_a?(String) }
       raise ArgumentError, "can't have more than one primary (root) table" if config.size > 1
 
-      @root, @config = config.keys.first, config.values.first
+      @root, @config = config.first.at(0), config.first.at(1)
 
       @input, @output = options.values_at(:input, :output)
     end
